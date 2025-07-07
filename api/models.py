51576c33d59a0ecd6649a89dcf5a1e51a45388b7
eigengrau-e <eigengrau01e@gmail.com>
2025-07-07 16:49:24 +0300

    Add tastipie to INSTALLED_APPS for create and use ApiKeys. Create hydrate and dehydrate methods for use category_id column when use GET, POST

--- api/models.py
+++ api/models.py
@@ -4,11 +4,13 @@ from shop.models import Category, Course
 from tastypie.authorization import Authorization
 from .authentication import CustomAuthentication
 
+
 class CategoryResource(ModelResource):
     class Meta:
         queryset = Category.objects.all()
         resource_name = 'categories'
         allowed_methods = ['get']
+
 
 class CourseResource(ModelResource):
     class Meta:
@@ -17,3 +19,11 @@ class CourseResource(ModelResource):
         allowed_methods = ['get', 'post', 'delete']
         authentication = CustomAuthentication()
         authorization = Authorization()
+
+    def hydrate(self, bundle):
+        bundle.obj.category_id = bundle.data['category_id']
+        return bundle
+
+    def dehydrate(self, bundle):
+        bundle.data['category_id'] = bundle.obj.category_id
+        return bundle
