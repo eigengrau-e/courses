1c1f5b798282f9d8ea7410413a206f4923b487bf
eigengrau-e <eigengrau01e@gmail.com>
2025-07-07 14:03:46 +0300

    Move dir templates to dir courses and Fix routes in shop/views

--- shop/views.py
+++ shop/views.py
@@ -5,7 +5,7 @@ from .models import Course
 
 def index(request):
     courses = Course.objects.all()
-    return render(request, 'courses.html', {'courses': courses})
+    return render(request, 'shop/courses.html', {'courses': courses})
 
 
 def single_course(request, course_id):
@@ -16,4 +16,4 @@ def single_course(request, course_id):
     #     raise Http404
 
     course = get_object_or_404(Course, pk=course_id)
-    return render(request, 'single_course.html', {'course': course})
+    return render(request, 'shop/single_course.html', {'course': course})
