3c387c80547d15e766a104e362db5dbca96a907d
eigengrau-e <eigengrau01e@gmail.com>
2025-07-07 12:09:51 +0300

    Create Template and View method for single_course

--- shop/views.py
+++ shop/views.py
@@ -6,3 +6,8 @@ from .models import Course
 def index(request):
     courses = Course.objects.all()
     return render(request, 'courses.html', {'courses': courses})
+
+
+def single_course(request, course_id):
+    course = Course.objects.get(pk = course_id)
+    return render(request, 'single_course.html', {'course': course})
