d5e0af91421cf1b530f0fe38366d08d89ccd07d3
eigengrau-e <eigengrau01e@gmail.com>
2025-07-07 18:51:57 +0300

    Add links to admin panel and API service in navbar. Change admin.py

--- base/urls.py
+++ base/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('shop.urls')),
     path('my-admin/', admin.site.urls),
-    path('shop/', include('shop.urls')),
     path('api/', include('api.urls')),
 ]
