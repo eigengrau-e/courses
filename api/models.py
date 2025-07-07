bedf30d8aad3214458eb050ee3d0d1d0209c1689
eigengrau-e <eigengrau01e@gmail.com>
2025-07-07 14:37:45 +0300

    Create CategoryResource and CourseResource, Add urlspatterns in base/urls for REST API service

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from tastypie.resources import ModelResource
 
-# Create your models here.
+from shop.models import Category, Course
+
+
+class CategoryResource(ModelResource):
+    class Meta:
+        queryset = Category.objects.all()
+        resource_name = 'categories'
+        allowed_methods = ['get']
+
+class CourseResource(ModelResource):
+    class Meta:
+        queryset = Course.objects.all()
+        resource_name = 'courses'
+        allowed_methods = ['get', 'post', 'delete']
